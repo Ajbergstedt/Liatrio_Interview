4ac25857ec9cb6046cfa09f37cca52dab5688ccc
ajbergstedt <ajbergstedt@gmail.com>
2025-03-18 21:49:12 -0700

    remove an dev pass throughs, add minification test

---
+++ .github/workflows/tests/minification.test.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const expect = require('chai').expect;
+
+describe('Tests for Minification', () => {
+  it('should return a minified JSON object with length less than 100 characters', async () => {
+    const res = await axios.get('http://localhost:80/');
+    const jsonLength = JSON.stringify(res.data).length;
+    expect(jsonLength).to.be.below(100, `JSON length (${jsonLength}) exceeds 100 characters`);
+  });
+});
